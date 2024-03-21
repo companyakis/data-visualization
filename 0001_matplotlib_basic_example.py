9676fc59bbe83dcf1d5c9186b6de54bd80ff4864
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2024-03-22 00:45:35 +0300

    Update 0001_matplotlib_basic_example.py

--- 0001_matplotlib_basic_example.py
+++ 0001_matplotlib_basic_example.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
-x = np.arange(0, 100, 10)
+x = np.arange(0, 100, 10) 
 
-y = 2 * x ** 2 - 3
+y = 2 * x ** 2 - 3 
 
-print(x)
+print(x) # [ 0 10 20 30 40 50 60 70 80 90]
 
-print(y)
+print(y) # [   -3   197   797  1797  3197  4997  7197  9797 12797 16197]
 
 plt.plot(x, y, 'b') #blue
 
